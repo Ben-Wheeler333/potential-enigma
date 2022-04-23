dff16f31dc465c5d204f648816b5c99dd3e10e99
Ben-Wheeler333 <chemlovexx@icloud.com>
2022-04-22 21:55:09 -0500

    deleted extra node_modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,10 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = ["What is the name of your project?", "Write a description for your project.", "Steps required to install your project?", "Provide examples for use.", "Give credits.", "List liscense if applicable."];
+const [projectName, projectDescription, installSteps, useExamples, credits, liscense ] = questions;
+console.log(projectName);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
