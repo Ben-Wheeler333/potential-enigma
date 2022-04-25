2fe805aecaaccdc83df820b53aabd0b4a30a5b8d
Ben-Wheeler333 <chemlovexx@icloud.com>
2022-04-24 20:16:34 -0500

    generateMarkdown commit

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./src/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = ["What is the name of your project?", "Write a description for your project.", "Steps required to install your project?", "Provide examples for use.", "Give credits.", "List liscense if applicable."];
-const [projectName, projectDescription, installSteps, useExamples, credits, liscense ] = questions;
-console.log(projectName);
+const questions = ["What is the name of your project?", "Write a description for your project.", "Steps required to install your project", "Provide examples for use.", "Give credits.", "List liscense if applicable."];
+const [projectName, projectDescription, steps, useExamples, credits, liscense ] = questions;
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const mdFile = generateMarkdown(data);
+
+fs.writeFile('README.md', generateMarkdown(), err => {
+    if (err) throw (err);
+
+    console.log('Your professional README file has been generated!');
+});
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: projectName
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: projectDescription
+        },
+        {
+            type: 'input',
+            name: 'steps',
+            message: steps
+        },
+        {
+            type: 'input',
+            name: 'examples',
+            message: useExamples
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: credits
+        },
+        {
+            type: 'input',
+            name: 'liscense',
+            message: liscense
+        }
+    ]);
+};
 
 // Function call to initialize app
-// init();
+ init()
+ .then(data => {
+     console.log(data)
+ });
